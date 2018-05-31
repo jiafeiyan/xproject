aceb817a7325bf9b4ba3f3bb462aad953de67b58
代思豪 <798814330@qq.com>
2018-05-31 13:11:17 +0800

    http头部增加token信息

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,19 +1,26 @@
 import axios from 'axios';
 
 let base = '';
-
-// let instance = axios.create({
-//     headers: {'content-type': 'application/x-www-form-urlencoded'},
-//     withCredentials: true
- 
-//   });
-
+let instance = axios.create();
+instance.interceptors.request.use(
+    config => {    
+    // 每次发送请求之前检测是否有token,如果有,放在请求头发送给服务器
+    var auth = sessionStorage.getItem("auth");
+    if (auth) {
+      config.headers.Authorization = auth;
+    }
+    return config
+    },
+    err => {  
+    return Promise.reject(err)
+    }
+)
 /**
  * 登录相关api
  * requestLogin--登录请求
  * 
  */
-export const requestLogin = params => { return axios.post(`/crossApi/login`, params).then(res => res.data); };
+export const requestLogin = params => { return instance.post(`/crossApi/login`, params).then(res => res); };
 //export const requestLogin = params => { return axios.post(`/crossApi/manager/login`, params).then(res => res.data); };
 
 
@@ -28,17 +35,17 @@ export const requestLogin = params => { return axios.post(`/crossApi/login`, par
  * getRcmListPage--分页查询推单
  * 
 */
-export const addRcm = params => { return axios.put(`/crossApi/manager/rcm/add`, { params: params }); };
+export const addRcm = params => { return instance.put(`/crossApi/manager/rcm/add`, { params: params }); };
 
-export const removeRcm = params => { return axios.delete(`/crossApi/manager/rcm/remove/${params}`)};
+export const removeRcm = params => { return instance.delete(`/crossApi/manager/rcm/remove/${params}`)};
 
-export const batchRemoveRcm = params => { return axios.delete(`/crossApi/manager/rcm/batchremove/`,{ params: params})};
+export const batchRemoveRcm = params => { return instance.delete(`/crossApi/manager/rcm/batchremove/`,{ params: params})};
 
-export const editRcm = params => { return axios.post(`/crossApi/manager/rcm/edit`, { params: params }); };
+export const editRcm = params => { return instance.post(`/crossApi/manager/rcm/edit`, { params: params }); };
 
-export const getRcmList = params => { return axios.get(`/crossApi/manager/rcm/list`, { params: params }); };
+export const getRcmList = params => { return instance.get(`/crossApi/manager/rcm/list`, { params: params }); };
 
-export const getRcmListPage = params => { return axios.get(`/crossApi/manager/rcm/listpage`, { params: params }); };
+export const getRcmListPage = params => { return instance.get(`/crossApi/manager/rcm/listpage`, { params: params }); };
 
 
 /** 
@@ -51,17 +58,17 @@ export const getRcmListPage = params => { return axios.get(`/crossApi/manager/rc
  * getOrgListPage--分页查询推单
  * 
 */
-export const addOrg = params => { return axios.put(`/crossApi/manager/org/add`, { params: params }); };
+export const addOrg = params => { return instance.put(`/crossApi/manager/org/add`, { params: params }); };
 
-export const removeOrg = params => { return axios.delete(`/crossApi/manager/org/remove/${params}`); };
+export const removeOrg = params => { return instance.delete(`/crossApi/manager/org/remove/${params}`); };
 
-export const batchRemoveOrg = params => { return axios.delete(`/crossApi/manager/org/batchremove`, { params: params }); };
+export const batchRemoveOrg = params => { return instance.delete(`/crossApi/manager/org/batchremove`, { params: params }); };
 
-export const editOrg = params => { return axios.post(`/crossApi/manager/org/edit`, { params: params }); };
+export const editOrg = params => { return instance.post(`/crossApi/manager/org/edit`, { params: params }); };
 
-export const getOrgList = params => { return axios.get(`/crossApi/manager/org/list`, { params: params }); };
+export const getOrgList = params => { return instance.get(`/crossApi/manager/org/list`, { params: params }); };
 
-export const getOrgListPage = params => { return axios.get(`/crossApi/manager/org/listpage`, { params: params }); };
+export const getOrgListPage = params => { return instance.get(`/crossApi/manager/org/listpage`, { params: params }); };
 
 
 /** 
@@ -74,15 +81,15 @@ export const getOrgListPage = params => { return axios.get(`/crossApi/manager/or
  * getBrdListPage--分页查询推单
  * 
 */
-export const addBrd = params => { return axios.put(`/crossApi/manager/brd/add`, { params: params }); };
+export const addBrd = params => { return instance.put(`/crossApi/manager/brd/add`, { params: params }); };
 
-export const removeBrd = params => { return axios.delete(`/crossApi/manager/brd/remove/${params}`); };
+export const removeBrd = params => { return instance.delete(`/crossApi/manager/brd/remove/${params}`); };
 
-export const batchRemoveBrd = params => { return axios.delete(`/crossApi/manager/brd/batchremove`, { params: params }); };
+export const batchRemoveBrd = params => { return instance.delete(`/crossApi/manager/brd/batchremove`, { params: params }); };
 
-export const editBrd = params => { return axios.post(`/crossApi/manager/brd/edit`, { params: params }); };
+export const editBrd = params => { return instance.post(`/crossApi/manager/brd/edit`, { params: params }); };
 
-export const getBrdList = params => { return axios.get(`/crossApi/manager/brd/list`, { params: params }); };
+export const getBrdList = params => { return instance.get(`/crossApi/manager/brd/list`, { params: params }); };
 
-export const getBrdListPage = params => { return axios.get(`/crossApi/manager/brd/listpage`, { params: params }); };
+export const getBrdListPage = params => { return instance.get(`/crossApi/manager/brd/listpage`, { params: params }); };
 
