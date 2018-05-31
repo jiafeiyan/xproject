3c1a20109cffd8e731674194e5d21013252f5ad6
chenyan <kimchy123@126.com>
2018-05-31 13:12:05 +0800

    修改token失效3小时

--- boot/boot-wechat/boot-wechat-auth/src/main/java/com/zeus/boot/jwt/entity/Security.java
+++ boot/boot-wechat/boot-wechat-auth/src/main/java/com/zeus/boot/jwt/entity/Security.java
@@ -4,5 +4,5 @@ public class Security {
     public final static String header = "Authorization";
     public final static String secret = "spring-security-@Jwt!&Secret^#";
     public final static String tokenHead = "Bearer ";
-    public final static long expiration=  3600;
+    public final static long expiration=  10800;
 }
